f5e282c4fb5d278651a710b89d1d0b9b2a1c31ce
Rushindhra-Goud <2303A52199@sru.edu.in>
2026-04-11 09:08:09 +0530

    Delete findline.py

--- findline.py
+++
@@ -1,5 +0,0 @@
-with open('assistant.py', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for i, line in enumerate(lines, 1):
-    if 'user_echo(command)' in line:
-        print(i, line.rstrip())
